bc3b9b321772639d40469d542b60ab58f58580d4
Nilay Singh <nilay.singh@angelbroking.com>
2021-01-18 19:35:28 +0530

    Added swagger

--- rooms/views.py
+++ rooms/views.py
@@ -12,6 +12,19 @@ from rest_framework.decorators import api_view
 from .models import *
 from .serializers import *
 
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.decorators import api_view
+
+from drf_yasg import openapi
+
+# test_param = openapi.Parameter('test', openapi.IN_QUERY, description="test manual param", type=openapi.TYPE_BOOLEAN)
+user_response = openapi.Response('response description', HotelSerializer)
+
+# 'method' can be used to customize a single HTTP method of a view
+@swagger_auto_schema(method='get', responses={200: user_response})
+# 'methods' can be used to apply the same modification to multiple methods
+@swagger_auto_schema(methods=['post'], request_body=HotelSerializer)
 @api_view(['GET','POST'])
 def api_hotel_list_view(request):
     hotel=Hotel.objects.all()
@@ -25,10 +38,17 @@ def api_hotel_list_view(request):
         if serializer.is_valid():
             serializer.save()
             # return 'created Successfully'
-            return Response(data,status=status.HTTP_201_CREATED)   
+            return Response(serializer.data)   
         else:
             # return 'Creation not succesfull'
-            return Response(serializer.error,status=status.HTTP_404_NOT_FOUND)
+            return Response(serializer.error)
+
+user_response = openapi.Response('response description', RoomSerializer)
+
+# 'method' can be used to customize a single HTTP method of a view
+@swagger_auto_schema(method='get', responses={200: user_response})
+# 'methods' can be used to apply the same modification to multiple methods
+@swagger_auto_schema(methods=['post'], request_body=RoomSerializer)
 
 @api_view(['GET','POST']) 
 def api_room_list_view(request):
@@ -41,10 +61,17 @@ def api_room_list_view(request):
         serializer=RoomSerializer(data=request.data) 
         if serializer.is_valid():
             serializer.save()
-            return Response(data,status=status.HTTP_201_CREATED)
+            return Response(request.data)
         else:
-            return Response(serializer.error,status=status.HTTP_404_NOT_FOUND)
+            return Response(serializer.error)
 
+
+user_response = openapi.Response('response description', GuestSerializer)
+
+# 'method' can be used to customize a single HTTP method of a view
+@swagger_auto_schema(method='get', responses={200: user_response})
+# 'methods' can be used to apply the same modification to multiple methods
+@swagger_auto_schema(methods=['post'], request_body=GuestSerializer)
 @api_view(['GET','POST'])
 def api_guest_list_view(request):
     guest=Guest.objects.all()
@@ -54,9 +81,17 @@ def api_guest_list_view(request):
     if request.method =='POST':
         serializer=GuestSerializer(data=request.data)
         if serializer.is_valid():
-            return Response(data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data)
         else:
-            return Response(serializer.error, status=status.HTTP_404_NOT_FOUND)                    
+            return Response(serializer.error)    
+
+
+user_response = openapi.Response('response description', BookingSerializer)
+
+# 'method' can be used to customize a single HTTP method of a view
+@swagger_auto_schema(method='get', responses={200: user_response})
+# 'methods' can be used to apply the same modification to multiple methods
+@swagger_auto_schema(methods=['post'], request_body=BookingSerializer)                
 @api_view(['GET','POST'])
 def api_booking_list_view(request):
     booking=Booking.objects.all()
@@ -66,6 +101,6 @@ def api_booking_list_view(request):
     if request.methos=='POST':
         serializer=BookingSerializer(data=request.data)
         if serializer.is_valid():
-            return Response(data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data)
         else:
-            return Response(serializer.error, status=status.HTTP_404_NOT_FOUND) 
+            return Response(serializer.error) 
